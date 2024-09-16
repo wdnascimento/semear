cee27fe7a9430e5bb32a71dcb8f1af203b17c222
Wagner <wagnerinfo@hotmail.com>
2024-09-16 17:48:01 -0300

    Beta 0.0.4 - Frist Deploy

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -74,7 +74,7 @@
 
     {{-- ABOUT SECTION --}}
 
-    <section id="about" class="ftco-section ftco-no-pt ftc-no-pb">
+    <section id="about" class="ftco-section ftc-no-pb">
         <div class="container">
             <div class="row justify-content-center mb-5 pb-2">
                 <div class="col-md-8 text-center heading-section ftco-animate">
@@ -84,73 +84,21 @@
             </div>
 
             <div class="row">
-                <div class="col-md-5 order-md-last wrap-about py-5 wrap-about bg-light">
-                    <div class="text px-4 ftco-animate">
-            <h2 class="mb-4">Welcome to Kiddos Learning School</h2>
-                        <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
-                        <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. And if she hasn’t been rewritten, then they are still using her.</p>
-                        <p><a href="#" class="btn btn-secondary px-4 py-3">Read More</a></p>
-                    </div>
-                </div>
-                <div class="col-md-7 wrap-about py-5 pr-md-4 ftco-animate">
-        <h2 class="mb-4">What We Offer</h2>
-                    <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
-                    <div class="row mt-5">
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-security"></span></div>
-                                <div class="text">
-                                    <h3>Safety First</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-reading"></span></div>
-                                <div class="text">
-                                    <h3>Regular Classes</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-diploma"></span></div>
-                                <div class="text">
-                                    <h3>Certified Teachers</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-education"></span></div>
-                                <div class="text">
-                                    <h3>Sufficient Classrooms</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-jigsaw"></span></div>
-                                <div class="text">
-                                    <h3>Creative Lessons</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-kids"></span></div>
-                                <div class="text">
-                                    <h3>Sports Facilities</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-md-12 wrap-about py-5 pr-md-4 ftco-animate">
+                    <h2 class="mb-4">Quem Somos?</h2>
+                    <p class="text-justify">
+                        Bem-vindo à Educacao Semear!
+                        <br><br>
+                        Na Educacao Semear, nossa missão é nutrir o potencial das crianças desde os primeiros dias de vida. Oferecemos um ambiente acolhedor e enriquecedor que vai do berçário ao Infantil 4, com o objetivo de apoiar o desenvolvimento integral dos pequenos em suas fases iniciais.
+                        <br>
+                        Nossa abordagem é fundamentada na compreensão de que cada criança é única e se desenvolve no seu próprio ritmo. Por isso, nossos programas são projetados para atender às necessidades específicas de cada faixa etária, promovendo o crescimento cognitivo, emocional e social através de atividades lúdicas e estimulantes.
+                        <br>
+                        Contamos com uma equipe de profissionais dedicados e experientes, que trabalham com carinho e empenho para criar experiências educativas que são tanto educativas quanto divertidas. Nosso currículo é cuidadosamente elaborado para garantir que as crianças explorem, descubram e aprendam de forma segura e prazerosa.
+                        <br>
+                        Na Educacao Semear, acreditamos que os primeiros anos são fundamentais para a formação de uma base sólida para o futuro. Por isso, nos empenhamos em proporcionar um ambiente onde cada criança possa florescer e se preparar para os próximos desafios com confiança e alegria.
+                        <br>
+                        Venha conhecer a nossa abordagem e descubra como podemos apoiar o desenvolvimento dos seus filhos desde os primeiros passos até o Infantil 4.
+                    </p>
                 </div>
             </div>
         </div>
